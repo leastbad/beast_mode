449896c19c93e18431f965f18869c0882ddf7597
leastbad <hello@leastbad.com>
2021-05-15 16:05:12 -0400

    morph customers

--- app/reflexes/customers_reflex.rb
+++ app/reflexes/customers_reflex.rb
@@ -60,7 +60,7 @@ class CustomersReflex < ApplicationReflex
     filter = CustomerFilter.find(element.dataset.filter)
     yield filter
     pagy, customers = pagy(filter.scope, page: filter.page, items: filter.items)
-    morph "#customers", render_collection(customers)
+    morph customers
     morph "#paginator", render(partial: "customers/paginator", locals: {pagy: pagy, filter: filter})
   end
 end
