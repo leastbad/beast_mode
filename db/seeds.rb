48bde404f85acf97d0c4ce5c9869f82134d105fa
Justin Marsh <jmarsh24@gmail.com>
2022-03-25 12:47:57 +0100

    Add bio to markdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
-unless Customer.exists?
-  125.times do
-    Customer.create! name: Faker::Name.name, email: Faker::Internet.email, company: Faker::Company.name, age: rand(18..70), status: ["Active", "Inactive", "Pending", "Suspended"].sample
+50.times do
+  [].tap do |array|
+    10_000.times do
+      time = Time.current
+      array << {
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        bio: Faker::Quote.most_interesting_man_in_the_world,
+        company: Faker::Company.name,
+        age: rand(18..70),
+        status: %w[Active Inactive Pending Suspended].sample,
+        updated_at: time,
+        created_at: time
+      }
+    end
+    Customer.insert_all(array)
   end
 end
